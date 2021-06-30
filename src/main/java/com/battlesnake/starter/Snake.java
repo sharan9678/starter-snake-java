8852c72f44e20cc91d872c3eb40b1c6152717d7e
sharan raj <sharan9678@gmail.com>
2021-06-30 22:08:31 +0530

    updated my info

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -101,7 +101,7 @@ public class Snake {
         public Map<String, String> index() {         
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
-            response.put("author", "");           // TODO: Your Battlesnake Username
+            response.put("author", "Sharan Raj");           // TODO: Your Battlesnake Username
             response.put("color", "#888888");     // TODO: Personalize
             response.put("head", "default");  // TODO: Personalize
             response.put("tail", "default");  // TODO: Personalize
