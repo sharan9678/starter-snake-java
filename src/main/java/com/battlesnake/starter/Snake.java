6727d681f41c72f89eb571af3b6c7c58de123c01
sharan raj <sharan9678@gmail.com>
2021-06-30 22:35:27 +0530

    updated snake color

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -102,7 +102,7 @@ public class Snake {
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
             response.put("author", "Sharan Raj");           // TODO: Your Battlesnake Username
-            response.put("color", "#ff5900");     // TODO: Personalize
+            response.put("color", "#888888");     // TODO: Personalize
             response.put("head", "default");  // TODO: Personalize
             response.put("tail", "default");  // TODO: Personalize
             return response;
